848ab57bc59bf69506414f5e2abcfba9d19afbdc
Darwin Subramaniam <darwinsubramaniam@googlemail.com>
2025-06-26 17:06:18 +0800

    test

--- tests/dags/tutorial.py
+++ tests/dags/tutorial.py
@@ -29,7 +29,7 @@ import textwrap
 from datetime import datetime, timedelta
 
 # Operators; we need this to operate!
-from airflow.providers.standard.operators.bash import BashOperator
+from airflow.operators.bash import BashOperator
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow.sdk import DAG
